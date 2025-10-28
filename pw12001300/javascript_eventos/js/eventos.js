699059aae1f7a972e816951bff32974177ac970d
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2025-10-28 12:47:31 -0700

    Implement drag-and-drop and list item addition

---
+++ pw12001300/javascript_eventos/js/eventos.js
@@ -0,0 +1,39 @@
+let elemento = document.querySelector('#elemento');
+let agregar=document.querySelector('#agregar');
+let listar=document.querySelector('#listadinamica');
+elemento.focus();
+
+agregar.addEventListener("click", () => {
+    if(elemento.value.trim()!="") {
+    let nuevoElemento = document.createElement("li");
+    nuevoElemento.innerText = elemento.value;
+    listar.appendChild(nuevoElemento);
+    elemento.value = "";
+    elemento.focus();
+    }
+});
+
+elemento.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter"){
+        agregar.click();
+    }
+});
+
+
+//arrastrar y soltar
+let arrastra = document.querySelector('#arrastra');
+let zonasoltar = document.getElementById('zonasoltar');
+
+arrastra.addEventListener("dragstart",function(e){
+    this.style.border="5px solid red";
+});
+
+zonasoltar.addEventListener("dragover",function(e){
+    //zonasoltar olvida que no se pueden
+    //arrastrar elementos sobre el
+    event.preventDefault();
+});
+zonasoltar.addEventListener("drop",function(e){
+    this.append(arrastra);
+    arrastra.style.border="none";
+});
