ea0a052e9e186a11ebb7e2ef3aa9d0317d2fa115
Jose Alonso Cibrian Robles <125108989+CibrianA04@users.noreply.github.com>
2025-11-03 12:56:30 -0700

    Add files via upload

---
+++ pw12001300/Typescript/calculadora/multiplicacion.ts
@@ -0,0 +1,10 @@
+const multiplicar = (a,b,imprimir) => {
+    console.log(imprimir, a * b);
+}
+multiplicar(2, 4, "El resultado es: "); 
+
+//instalar node.js
+//npm init -y
+//npm install --save-dev typescript ts-node
+//borrar node modules
+//npm install <---- para regenerar node_modules
