cc648663a9d44e55db979c0da65b26545a9059a6
Jose Alonso Cibrian Robles <l22170613@culiacan.tecnm.mx>
2025-11-10 12:54:30 -0700

    Backend

---
+++ BACKEND/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//API --- BACKEND
+//NODEJS --- EXPRESS
+//npm install express -E
+//npm install ts-node-dev -D  esto es para desarrollo con typescript sin compilar
---
+++ BACKEND/src/index.ts
@@ -0,0 +1,7 @@
+//API --- BACKEND
+//NODEJS --- EXPRESS
+//npm install express -E
+//npm install ts-node-dev -D  esto es igual que -save-dev 
+//npm install --save-dev cors 
+//npm i --save-dev @types/express
+import express from 'express'; 
