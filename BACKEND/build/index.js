70d78985bd43c1ed37a2ab054407f75d89cbf88a
Jose Alonso Cibrian Robles <l22170613@culiacan.tecnm.mx>
2025-11-11 12:52:03 -0700

    nodemon

--- BACKEND/build/index.js
+++ BACKEND/build/index.js
@@ -1,6 +1,19 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//API --- BACKEND
-//NODEJS --- EXPRESS
-//npm install express -E
-//npm install ts-node-dev -D  esto es para desarrollo con typescript sin compilar
+import express from 'express';
+//Creamos la aplicacion a traves del paquete express
+//y llamamos a su constructor
+const app = express();
+//todo lo que regresa al usuario es en formato json
+app.use(express.json());
+//Para escuchar las peticiones del frontend
+const PUERTO = 3001;
+//Rutas
+app.get('/', (_req, res) => {
+    res.send('Estoy en la raiz del servidor API');
+});
+app.get('/Hola', (_req, res) => {
+    res.send('Estoy en Hola del servidor API');
+});
+//encendemos el servidor y lo ponemos a escuchar peticiones
+app.listen(PUERTO, () => {
+    console.log(`Servidor encendido escuchando el puerto: ${PUERTO}`);
+});
--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,7 +1,23 @@
-//API --- BACKEND
-//NODEJS --- EXPRESS
-//npm install express -E
-//npm install ts-node-dev -D  esto es igual que -save-dev 
-//npm install --save-dev cors 
-//npm i --save-dev @types/express
 import express from 'express'; 
+//Creamos la aplicacion a traves del paquete express
+//y llamamos a su constructor
+const app = express();
+//todo lo que regresa al usuario es en formato json
+app.use(express.json());
+//Para escuchar las peticiones del frontend
+const PUERTO:number = 3001;
+
+//Rutas
+app.get('/',(_req, res)=>{
+    res.send('Estoy en la raiz del servidor API');
+})
+app.get('/Hola',(_req, res)=>{
+    res.send('Estoy en Hola del servidor API');
+})
+
+//encendemos el servidor y lo ponemos a escuchar peticiones
+app.listen(PUERTO,()=>{
+    console.log(`Servidor encendido escuchando el puerto: ${PUERTO}`);
+})
+
+
