e2a7efe3f66bd8d78978f9d4d5d47f2e7a1d8e64
Jose Alonso Cibrian Robles <l22170613@culiacan.tecnm.mx>
2025-11-24 13:01:00 -0700

    agregando interface, api, modulos y controladores

---
+++ FRONTEND/cliente_tienda/src/modulos/principal/personal/personal/vistas/personalVue.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h3></h3>
+        <h3>Personal</h3>
+        <div>
+            <button class="btn btn-sm btn-outline-primary">Agregar</button>
+        </div>
+    </section>
+    <table class = "table table-striped">
+        <thead>
+            <th>Clave
+            </th>
+            <th>Nombre
+            </th>
+            <th>telefono
+            </th>
+            <th>Estatus</th>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
